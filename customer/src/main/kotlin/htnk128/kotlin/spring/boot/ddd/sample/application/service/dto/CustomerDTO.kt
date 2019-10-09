253b9fad5c20f65551d2c3bab8f7b18aef774512
tanaka_hiroaki <tanaka_hiroaki@cyberagent.co.jp>
2019-10-09 17:51:01 +0900

    [REFACT]パッケージ移動

--- customer/src/main/kotlin/htnk128/kotlin/spring/boot/ddd/sample/application/service/dto/CustomerDTO.kt
+++
@@ -1,6 +0,0 @@
-package htnk128.kotlin.spring.boot.ddd.sample.application.service.dto
-
-data class CustomerDTO(
-    val customerId: String,
-    val name: String
-)
--- customer/src/main/kotlin/htnk128/kotlin/spring/boot/ddd/sample/domain/shared/ValueObject.kt
+++
@@ -1,6 +0,0 @@
-package htnk128.kotlin.spring.boot.ddd.sample.domain.shared
-
-interface ValueObject<T> {
-
-    fun sameValueAs(other: T): Boolean
-}
