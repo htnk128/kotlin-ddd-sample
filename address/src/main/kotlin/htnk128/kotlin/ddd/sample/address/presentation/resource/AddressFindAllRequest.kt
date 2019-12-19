ca124b15b36b14fda01a201d4161cbca9bc4c6a2
hiroakitanaka128 <tanaka_hiroaki@cyberagent.co.jp>
2019-12-20 01:07:16 +0900

    [UPDATE]update comment

--- address/src/main/kotlin/htnk128/kotlin/ddd/sample/address/presentation/resource/AddressFindAllRequest.kt
+++ address/src/main/kotlin/htnk128/kotlin/ddd/sample/address/presentation/resource/AddressFindAllRequest.kt
@@ -7,7 +7,7 @@ import io.swagger.annotations.ApiModelProperty
  */
 data class AddressFindAllRequest(
     @ApiModelProperty(
-        value = "アカウントのID", example = "CUST_c5fb2cec-a77c-4886-b997-ffc2ef060e78", required = true, position = 1
+        value = "アカウントのID", example = "AC_c5fb2cec-a77c-4886-b997-ffc2ef060e78", required = true, position = 1
     )
     val accountId: String
 )
