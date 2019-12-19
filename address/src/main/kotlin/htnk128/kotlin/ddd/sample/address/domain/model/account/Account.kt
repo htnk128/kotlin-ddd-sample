708e593d8387f3e37f6312ab05d45c416a994283
tanaka_hiroaki <tanaka_hiroaki@cyberagent.co.jp>
2019-12-19 16:35:45 +0900

    [ADD]add column

--- address/src/main/kotlin/htnk128/kotlin/ddd/sample/address/domain/model/account/Account.kt
+++ address/src/main/kotlin/htnk128/kotlin/ddd/sample/address/domain/model/account/Account.kt
@@ -1,13 +1,22 @@
 package htnk128.kotlin.ddd.sample.address.domain.model.account
 
 import htnk128.kotlin.ddd.sample.dddcore.domain.Entity
+import java.time.Instant
 
 /**
  * アカウントのコンテキストマッピングを表現する。
  */
 class Account(
-    val accountId: AccountId
+    val accountId: AccountId,
+    val createdAt: Instant,
+    val deletedAt: Instant?,
+    val updatedAt: Instant
 ) : Entity<Account> {
+
+    /**
+     * このアカウントが削除されている場合に`true`を返す。
+     */
+    val isDeleted: Boolean = deletedAt != null
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
