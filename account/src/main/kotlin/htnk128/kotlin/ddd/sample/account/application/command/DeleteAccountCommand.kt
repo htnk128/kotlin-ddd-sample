0f655972199384dcecad0c8bb9b370d3b7271fd0
htnk128 <hiroaki.tanaka128@gmail.com>
2021-06-02 14:59:30 +0900

    [UPDATE] Update package

--- account/src/main/kotlin/htnk128/kotlin/ddd/sample/account/application/command/DeleteAccountCommand.kt
+++
@@ -1,8 +0,0 @@
-package htnk128.kotlin.ddd.sample.account.application.command
-
-/**
- * アカウントを削除する際のコマンド情報。
- */
-data class DeleteAccountCommand(
-    val accountId: String
-)
--- account/src/main/kotlin/htnk128/kotlin/ddd/sample/account/application/command/FindAccountCommand.kt
+++
@@ -1,8 +0,0 @@
-package htnk128.kotlin.ddd.sample.account.application.command
-
-/**
- * アカウントを取得する際のコマンド情報。
- */
-data class FindAccountCommand(
-    val accountId: String
-)
